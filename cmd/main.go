9015cbed363dd52667a2135aee0f52e0086258ef
cuimingda <cuimingda@gmail.com>
2024-12-22 15:27:23 +0800

    test

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
+	"flag"
+	"fmt"
+	"os"
 )
 
 const version = "1.0.0"
 
 func main() {
-    var showVersion bool
-    var shortVersion bool
+	var showVersion bool
+	var shortVersion bool
 
-    flag.BoolVar(&showVersion, "version", false, "显示版本信息")
-    flag.BoolVar(&shortVersion, "v", false, "显示版本信息")
-    flag.Parse()
+	flag.BoolVar(&showVersion, "version", false, "显示版本信息")
+	flag.BoolVar(&shortVersion, "v", false, "显示版本信息")
+	flag.Parse()
 
-    if showVersion || shortVersion {
-        fmt.Println("development-environment-cli 版本:", version)
-        os.Exit(0)
-    }
+	if showVersion || shortVersion {
+		fmt.Println("development-environment-cli 版本:", version)
+		os.Exit(0)
+	}
 
-    fmt.Println("hello world")
+	fmt.Println("hello world again")
 }
