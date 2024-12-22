395e755393bf11aefa1cb990bb807d97d054667c
cuimingda <cuimingda@gmail.com>
2024-12-22 17:06:09 +0800

    version check in doctor

--- cmd/doctor.go
+++ cmd/doctor.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 )
 
-func HandleDoctorCommand(args []string) {
-	// 输出参数
-	fmt.Println("hello doctor")
-	for _, arg := range args {
-		fmt.Println(arg)
+func HandleDoctorCommand() {
+
+	// 检查命令
+	checkCommand("docker", "--version")
+	checkCommand("git", "--version")
+	checkCommand("node", "--version")
+	checkCommand("npm", "--version")
+	checkCommand("curl", "--version")
+	checkCommand("gh", "--version")
+	checkCommand("go", "version")
+}
+
+func checkCommand(name string, arg string) {
+	cmd := exec.Command(name, arg)
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("\033[31m%s未安装或无法执行%s命令\033[0m\n", name, name) // 红色
+	} else {
+		fmt.Printf("\033[32m%s版本: %s\033[0m\n", name, output) // 绿色
 	}
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,20 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
 func main() {
-    // 定义子命令
-    doctorCmd := flag.NewFlagSet("doctor", flag.ExitOnError)
 
-    // 检查子命令
-    if len(os.Args) > 1 {
-        switch os.Args[1] {
-        case "doctor":
-            doctorCmd.Parse(os.Args[2:])
-            HandleDoctorCommand(doctorCmd.Args())
-            return
-        }
-    }
+	// 检查子命令
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "doctor":
+			HandleDoctorCommand()
+			return
+		}
+	}
 
-    fmt.Println("hello world")
+	fmt.Println("hello world")
 }
