a576bc8619a9bd220be3714d8edf8ff2baa0ff98
cuimingda <cuimingda@gmail.com>
2024-12-24 22:18:59 +0800

    add no-port mode

--- cmd/web.go
+++ cmd/web.go
@@ -11,7 +11,7 @@ import (
 func HandleWebCommand(args []string) {
 
 	webCmd := flag.NewFlagSet("web", flag.ExitOnError)
-	port := webCmd.String("port", "5173", "端口号")
+	port := webCmd.String("port", "", "端口号")
 
 	webCmd.Parse(args)
 
@@ -20,13 +20,20 @@ func HandleWebCommand(args []string) {
 		log.Fatalf("获取当前工作目录失败: %v", err)
 	}
 	log.Printf("当前工作目录: %s", workingDir)
-	log.Printf("端口号: %s", *port)
 
 	// 获取当前工作目录的名称，类似于 shell 中的 basename
 	dirName := filepath.Base(workingDir)
 	log.Printf("当前目录名称: %s", dirName)
 
-	cmd := exec.Command("docker", "run", "-it", "--rm", "-w", "/app", "-v", workingDir+":/app", "--publish", *port+":"+*port, "--network", "local-network", "--name", dirName+"-container", "cuimingda/development-environment")
+	var cmd *exec.Cmd
+
+	if *port == "" {
+		log.Println("端口号: 未设置")
+		cmd = exec.Command("docker", "run", "-it", "--rm", "-w", "/app", "-v", workingDir+":/app", "--name", dirName+"-container", "cuimingda/development-environment")
+	} else {
+		log.Printf("端口号: %s", *port)
+		cmd = exec.Command("docker", "run", "-it", "--rm", "-w", "/app", "-v", workingDir+":/app", "--publish", *port+":"+*port, "--network", "local-network", "--name", dirName+"-container", "cuimingda/development-environment")
+	}
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
