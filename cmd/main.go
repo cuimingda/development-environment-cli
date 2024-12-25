ddc1cfc71bceb81278545eed1f089fd253352424
cuimingda <cuimingda@gmail.com>
2024-12-25 12:02:26 +0800

    add debugOsArgs

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"reflect"
 )
 
+func debugOsArgs() {
+	log.Printf("typeof of os.Args - %s\n", reflect.TypeOf(os.Args))
+
+	log.Printf("len(os.Args) - %d", len(os.Args))
+	for i, arg := range os.Args {
+		log.Printf("os.Args[%d] - %s", i, arg)
+	}
+}
+
 func main() {
+
+	debugOsArgs()
 
 	// 检查子命令
 	if len(os.Args) > 1 {
