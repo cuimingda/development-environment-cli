ecca36b1f730be8a9929599c555125bd9fd057a7
cuimingda <cuimingda@gmail.com>
2024-12-25 13:41:42 +0800

    add default dev command

--- cmd/main.go
+++ cmd/main.go
@@ -26,10 +26,15 @@ func main() {
 		case "web":
 			HandleWebCommand(os.Args[2:])
 			return
+		default:
+			message := fmt.Sprintf("[ERROR] 未知命令 - %s", os.Args[1])
+			colorizeMessage := colorize(message, "red")
+			log.Fatalln(colorizeMessage)
+			return
 		}
 	}
 
-	fmt.Println("hello world")
+	handleDevCommand()
 }
 
 func debugOsArgs() {
@@ -50,38 +55,79 @@ func fatalIfNotExistDir(dir string) {
 	}
 }
 
-func HandleWebCommand(args []string) {
-
-	webCmd := flag.NewFlagSet("web", flag.ExitOnError)
-	port := webCmd.String("port", "", "端口号")
-
-	webCmd.Parse(args)
-
+func getWorkingDirOrFatal() string {
 	workingDir, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("获取当前工作目录失败: %v", err)
 	}
 	log.Printf("当前工作目录: %s", workingDir)
 
+	return workingDir
+}
+
+func getWorkingDirName() string {
+	workingDir := getWorkingDirOrFatal()
+
 	// 获取当前工作目录的名称，类似于 shell 中的 basename
 	dirName := filepath.Base(workingDir)
 	log.Printf("当前目录名称: %s", dirName)
 
-	var cmd *exec.Cmd
+	return dirName
+}
 
-	if *port == "" {
-		log.Println("端口号: 未设置")
-		cmd = exec.Command("docker", "run", "-it", "--rm", "-w", "/app", "-v", workingDir+":/app", "--name", dirName+"-container", "cuimingda/development-environment")
-	} else {
-		log.Printf("端口号: %s", *port)
-		cmd = exec.Command("docker", "run", "-it", "--rm", "-w", "/app", "-v", workingDir+":/app", "--publish", *port+":"+*port, "--network", "local-network", "--name", dirName+"-container", "cuimingda/development-environment")
-	}
+func handleDevCommand() {
+
+	workingDir := getWorkingDirOrFatal()
+	dirName := getWorkingDirName()
+
+	cmd := exec.Command(
+		"docker", "run",
+		"-it",
+		"--rm",
+		"-w", "/app",
+		"-v", workingDir+":/app",
+		"--name", dirName+"-container",
+		"cuimingda/development-environment",
+	)
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	err = cmd.Run()
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("运行 Alpine Linux 容器失败: %v", err)
+	}
+}
+
+func HandleWebCommand(args []string) {
+
+	webCmd := flag.NewFlagSet("web", flag.ExitOnError)
+	port := webCmd.String("port", "5173", "端口号")
+
+	webCmd.Parse(args)
+
+	workingDir := getWorkingDirOrFatal()
+	dirName := getWorkingDirName()
+
+	log.Printf("端口号: %s", *port)
+	cmd := exec.Command(
+		"docker", "run",
+		"-it",
+		"--rm",
+		"-w", "/app",
+		"-v", workingDir+":/app",
+		"--publish", *port+":"+*port,
+		"--network", "local-network",
+		"--name", dirName+"-container",
+		"cuimingda/development-environment",
+	)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
 	if err != nil {
 		log.Fatalf("运行 Alpine Linux 容器失败: %v", err)
 	}
