89b539ace7a21c988cbcd96fd4ac783aaa7561e8
cuimingda <cuimingda@gmail.com>
2024-12-30 11:00:04 +0800

    combine two build command

--- internal/cmd/build.go
+++ internal/cmd/build.go
@@ -2,13 +2,10 @@ package cmd
 
 import (
 	"development-environment-cli/internal/utils"
+	"fmt"
 
 	"github.com/spf13/cobra"
 )
-
-func init() {
-	rootCmd.AddCommand(buildCommand)
-}
 
 var buildCommand = &cobra.Command{
 	Use:   "build",
@@ -17,16 +14,40 @@ var buildCommand = &cobra.Command{
 		handleBuildCommand()
 	},
 }
+var DisableLocalBinary bool
+var DisableDockerImage bool
+
+func init() {
+	rootCmd.AddCommand(buildCommand)
+	buildCommand.Flags().BoolVarP(&DisableLocalBinary, "disable-local-binary", "", false, "not to build to local bin")
+	buildCommand.Flags().BoolVarP(&DisableDockerImage, "disable-docker-image", "", false, "not to build docker image")
+}
 
 func handleBuildCommand() {
 
 	printVerboseMessage("开始构建")
 
-	utils.EnsureMacOS()
-	utils.EnsurePath(".git")
-	utils.EnsureCommand("go")
+	if Verbose {
+		fmt.Printf("DisableLocalBinary: %v\n", DisableLocalBinary)
+		fmt.Printf("DisableDockerImage: %v\n", DisableDockerImage)
+	}
 
-	utils.ExecuteCommand("go", "build", "-o", "bin/dev", ".")
+	utils.EnsurePath(".git")
+	utils.EnsurePath("Dockerfile")
+	utils.EnsureGitRemoteOrigin("git@github.com:cuimingda/development-environment-cli.git")
+
+	if !DisableLocalBinary {
+		utils.EnsurePath("main.go")
+		utils.ExecuteCommand("go", "build", "-o", "bin/dev", ".")
+	} else {
+		fmt.Println("跳过构建本地二进制文件")
+	}
+
+	if !DisableDockerImage {
+		utils.ExecuteCommand("docker", "build", "--tag", "cuimingda/development-environment:latest", ".")
+	} else {
+		fmt.Println("跳过构建 Docker 镜像")
+	}
 
 	printVerboseMessage("构建完成")
 }
