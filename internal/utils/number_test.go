7e14429cdab71e66fc89dab1be8629b104bfecac
cuimingda <cuimingda@gmail.com>
2025-01-09 16:05:00 +0800

    add more testcases

--- internal/utils/number_test.go
+++ internal/utils/number_test.go
@@ -5,7 +5,12 @@ import "testing"
 func TestEnsureIntegerRange(t *testing.T) {
 	tests := []TestCase{
 		{EnsureIntegerRange, true, []interface{}{5, 1, 10}},
+		{EnsureIntegerRange, true, []interface{}{1, 1, 10}},
+		{EnsureIntegerRange, true, []interface{}{10, 1, 10}},
 		{EnsureIntegerRange, false, []interface{}{11, 1, 10}},
+		{EnsureIntegerRange, false, []interface{}{0, 1, 10}},
+		{EnsureIntegerRange, false, []interface{}{-1, 1, 10}},
+		{EnsureIntegerRange, false, []interface{}{5, 10, 1}},
 	}
 
 	RunTests(t, tests)
